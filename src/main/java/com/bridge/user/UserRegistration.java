92c21d1da07d191e1aa697bbde9c3bd58a0465b3
deepakphpweb <dipakrana2760@gmail.com>
2021-12-16 19:32:33 +0530

    Added : Welcome To Junit User Registration Problem

---
+++ src/main/java/com/bridge/user/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.bridge.user;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Junit User Registration Problem");
+    }
+}
