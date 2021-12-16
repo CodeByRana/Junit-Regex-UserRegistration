d856855f766cb2d493e68ad7d23c7f69f443914a
deepakphpweb <dipakrana2760@gmail.com>
2021-12-16 19:40:40 +0530

    Added : dependencies in pom.xml file

--- src/main/java/com/bridge/user/UserRegistration.java
+++ src/main/java/com/bridge/user/UserRegistration.java
@@ -1,7 +1,11 @@
 package com.bridge.user;
 
+import java.util.Scanner;
+
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome To Junit User Registration Problem");
+
+
     }
 }
