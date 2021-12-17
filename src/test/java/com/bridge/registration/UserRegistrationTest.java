a0c6dc8dfbb604eee028bfa25113f1e26098df2d
deepakphpweb <dipakrana2760@gmail.com>
2021-12-17 22:05:27 +0530

    Added :As a USer need to follow Predefind Password Rule-minimum 8 character

--- src/test/java/com/bridge/registration/UserRegistrationTest.java
+++ src/test/java/com/bridge/registration/UserRegistrationTest.java
@@ -1,6 +1,6 @@
 package com.bridge.registration;
 
-import com.bridge.user.RegexClass;
+import com.bridge.user.RegexClassValidation;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,69 +9,69 @@ public class UserRegistrationTest {
         @Test
         public void givenFirstName_CaseOne_WhenFirstNameCorrect_ShouldReturnValid(){
 
-                Assert.assertTrue(RegexClass.RegexFunctionFirstname("Deepak"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionFirstname("Deepak"));
         }
         @Test
         public void givenFirstName_CaseTwo_WhenFirstNameCorrect_ShouldReturnValid() {
 
-                Assert.assertTrue(RegexClass.RegexFunctionFirstname("deepak"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionFirstname("deepak"));
         }
         @Test
         public void givenFirstName_CaseThree_WhenFirstNameCorrect_ShouldReturnValid() {
 
-                Assert.assertTrue(RegexClass.RegexFunctionFirstname("Dee"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionFirstname("Dee"));
         }
         @Test
         public void givenFirstName_CaseFour_WhenFirstNameCorrect_ShouldReturnValid() {
 
-                Assert.assertTrue(RegexClass.RegexFunctionFirstname("de"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionFirstname("de"));
         }
         //Test Case for Last Name
         @Test
         public void givenLastName_CaseOne_WhenLastNameCorrect_ShouldReturnValid(){
 
-                Assert.assertTrue(RegexClass.RegexFunctionLastname("Rana"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionLastname("Rana"));
         }
         @Test
         public void givenLastName_CaseTwo_WhenLastNameCorrect_ShouldReturnValid() {
 
-                Assert.assertTrue(RegexClass.RegexFunctionLastname("rana"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionLastname("rana"));
         }
         @Test
         public void givenLastName_CaseThree_WhenLastNameCorrect_ShouldReturnValid() {
 
-                Assert.assertTrue(RegexClass.RegexFunctionLastname("Ran"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionLastname("Ran"));
         }
         @Test
         public void givenLastName_CaseFour_WhenLastNameCorrect_ShouldReturnValid() {
 
-                Assert.assertTrue(RegexClass.RegexFunctionLastname("ra"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionLastname("ra"));
         }
 
         //Test Case for Email Address
         @Test
         public void givenEmailId_CaseOno_WhenEmailIdCorrect_ShouldReturnValidId() {
 
-                Assert.assertTrue(RegexClass.RegexFunctionEmailId("dipak@gmail.com"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionEmailId("dipak@gmail.com"));
         }
         @Test
         public void givenEmailId_CaseTwo_WhenEmailIdCorrect_ShouldReturnValidId() {
 
-                Assert.assertTrue(RegexClass.RegexFunctionEmailId("dipak@gmail.com.in"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionEmailId("dipak@gmail.com.in"));
         }
         @Test
         public void givenEmailId_CaseThree_WhenEmailIdCorrect_ShouldReturnValidId() {
 
-                Assert.assertTrue(RegexClass.RegexFunctionEmailId("dipak.123@gmail.com"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionEmailId("dipak.123@gmail.com"));
         }
         @Test
         public void givenEmailId_CaseFour_WhenEmailIdCorrect_ShouldReturnValidId() {
 
-                Assert.assertTrue(RegexClass.RegexFunctionEmailId("rana.deepak@gmail.com"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionEmailId("rana.deepak@gmail.com"));
         }
         //Test Case for Mobile Number
         @Test
         public void givenMobileNo_CaseOne_WhenMobileNoCorrect_ShouldReturnValidMobNo(){
-                Assert.assertTrue(RegexClass.RegexFunctionEmailId("7011133639"));
+                Assert.assertTrue(RegexClassValidation.RegexFunctionEmailId("91 7011133639"));
         }
 }
